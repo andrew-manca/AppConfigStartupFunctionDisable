381a4d001e6f60181e06d4c80660bff191d13f5d
Andrew Manca <anmanca@microsoft.com>
2021-02-26 08:52:59 -0500

    initial commit

---
+++ AppConfigStartupDisableFunctions/startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+[assembly: FunctionsStartup(typeof(AppConfigStartupDisableFunctions.Startup))]
+
+namespace AppConfigStartupDisableFunctions
+{
+    class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            string connectionString = "{AppConfigConnString}";
+            builder.ConfigurationBuilder.AddAzureAppConfiguration(connectionString);
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
